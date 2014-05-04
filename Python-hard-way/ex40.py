b9f2894a78b83ca5c1ae9143d003406cd52503e0
leevs <lee@Lees-MacBook-Pro.local>
2014-05-04 15:36:22 +0200

    Not my projects but learning python

---
+++ Python-hard-way/ex40.py
@@ -0,0 +1,7 @@
+class Mystuff(object):
+  """docstring for ClassName"""
+  def __init__(self):
+    self.tangerine = "And now a thousand years between"
+
+  def apple(self):
+    print "I am classy Apples!"
